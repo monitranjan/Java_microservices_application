ed3bdf9741ff9b4a4131ec4ed66ac47c7e094c22
monitranjan <monitranjan2050@gmail.com>
2024-05-07 00:19:48 +0530

    configServer setup added

---
+++ configServer/src/test/java/com/monit/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monit.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
